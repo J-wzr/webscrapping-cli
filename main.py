5e627678322682eb8525a1a3e8ec9cf37e1c4a21
J-wzr <joshuamwesigwa58@gmail.com>
2023-12-01 23:18:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import smtplib
+import ssl
+import time
+import requests
+import selectorlib
+
+URL = "https://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    request = requests.get(url, headers=HEADERS)
+    source = request.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    SENDER = "joshuamwesigwa58@gmail.com"
+    PASSWORD = "lfdy hcrk wrpl orab"
+    RECEIVER = "annitahkyomuhendo25@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(SENDER, PASSWORD)
+        server.sendmail(SENDER, RECEIVER, message)
+
+    print("Email Sent")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey, A new event was found!")
+            time.sleep(2)
